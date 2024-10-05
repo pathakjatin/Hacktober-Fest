a9ba0f4e971f96d433bed49462a08a1052da332f
Jatin Pathak <140906335+pathakjatin@users.noreply.github.com>
2024-10-05 13:06:53 +0530

    cpp 27 med

--- C++/Medium/27.cpp
+++ C++/Medium/27.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 int main()
 {
-    string letters[2] = {
+    string letters[2][4] = {
         {"A", "B", "C", "D"},
         {"E", "F", "G", "H"}};
     int someDigits[2][5] = {
         {1, 2, 3, 4, 5},
-        {6, 7, 8, "A"}
-    } cout << letters[1][3]
-           << endl;
-    cout << someDigits[1][5] << endl return 0;
+        {6, 7, 8, 9, 10}
+    };
+    cout << letters[1][3]<< endl;
+    cout << someDigits[1][4] << endl;
+    return 0;
 }
