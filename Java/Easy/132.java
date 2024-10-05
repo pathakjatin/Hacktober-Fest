988b009d3e9291957feb6229e1ec6b95a4628e76
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-05 12:58:40 +0530

    Update 132.java
    Java Easy 132

--- Java/Easy/132.java
+++ Java/Easy/132.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         String s = "java";
-        s.replace('a', "o");
+        s.replace("a", "o");
         System.out.println(s);
     }
 }
