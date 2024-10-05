42f29a4e570aa25bd45b89f5aee3f020ab93782b
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:00:46 +0530

    49-java-easy

--- Java/Easy/49.java
+++ Java/Easy/49.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 class Main {
     public static void main(String[] args) {
-        List<int> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(10);
         System.out.println(numbers.get(0));
     }
