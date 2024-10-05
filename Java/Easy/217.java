a4643cefa9730b513abaedcccab78c02c94794ab
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-05 13:57:01 +0530

    Update 217.java
    Java Easy 217

--- Java/Easy/217.java
+++ Java/Easy/217.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         String text = "hello";
-        char ch = text.charAt(6);
+        char ch = text.charAt(4);
         System.out.println(ch);
     }
 }
