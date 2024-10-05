cd3e2b0b81d45af5440c250fa4078c64817b5d5a
Jatin Pathak <140906335+pathakjatin@users.noreply.github.com>
2024-10-05 13:37:41 +0530

    cpp 13 med

--- C++/Medium/13.cpp
+++ C++/Medium/13.cpp
@@ -5,8 +5,11 @@ int main()
     int x = 5;
     if (x > 3)
         if (x < 10)
+        {
             cout << "x is between 3 and 10" << endl;
-        else
+        }
+        else{
             cout << "x is greater than 10" << endl;
+        }
     return 0;
 }
