341c861306795eff136c099b382494fdbd2e5f82
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-05 12:38:57 +0530

    Update 51.java
    Java Easy

--- Java/Easy/51.java
+++ Java/Easy/51.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int number = 5;
-        if (number = 5) {
+        if (number == 5) {
             System.out.println("Number is 5");
         }
     }
