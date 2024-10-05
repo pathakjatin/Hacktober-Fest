bc4c2f36629b76829f057cae449d0a80d8b67078
Jatin Pathak <140906335+pathakjatin@users.noreply.github.com>
2024-10-05 12:53:59 +0530

    cpp 39 med

--- C++/Medium/39.cpp
+++ C++/Medium/39.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 int main()
 {
-    string someString = "Hello There" string new_string[] = "I wrote some shit";
-    String name[] = "John Doe" cout << someString << endl;
+    string someString = "Hello There";
+    string new_string = "I wrote some shit";
+    string name[] = {"John Doe"};
+    cout << someString << endl;
     cout << new_string << endl;
     cout << name[0] << endl;
 }
