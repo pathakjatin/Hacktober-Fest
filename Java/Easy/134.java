032b1fcde93399e3c75b8b34736f0d7a998df8f8
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-05 13:05:30 +0530

    Update 134.java
    Java Easy 134

--- Java/Easy/134.java
+++ Java/Easy/134.java
@@ -1,6 +1,7 @@
+import java.util.*;
 class Main {
     public static void main(String[] args) {
-        List<int> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(5);
         System.out.println(numbers.get(0));
     }
