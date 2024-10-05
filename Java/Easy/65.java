54bc2ccc6ec480edbf29fc895e0a2204cfb15c79
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:17:40 +0530

    65-easy-java

--- Java/Easy/65.java
+++ Java/Easy/65.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         double pi = 3.14159;
         double radius = 5.0;
-        double area = Math.pi * radius * radius;
+        double area = pi * radius * radius;
         System.out.println(area);
     }
 }
