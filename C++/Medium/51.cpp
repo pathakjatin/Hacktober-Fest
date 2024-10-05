a7975c50e96f05805ec5197af90d3bd4d6311445
Jatin Pathak <140906335+pathakjatin@users.noreply.github.com>
2024-10-05 12:34:05 +0530

    cpp med 51

--- C++/Medium/51.cpp
+++ C++/Medium/51.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int a = 5, b = 10;
-    if (a > 0 & b > 0)
+    if (a > 0 && b > 0)
     {
         cout << "Both a and b are positive." << endl;
     }
-    if (a > 0 | b < 0)
+    if (a > 0 || b < 0)
     {
         cout << "At least one condition is true." << endl;
     }
