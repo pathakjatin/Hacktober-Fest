733b07880c7ee28b2b307e1928108814716ca8f6
Jatin Pathak <140906335+pathakjatin@users.noreply.github.com>
2024-10-05 13:19:05 +0530

    cpp 60 med

--- C++/Medium/60.cpp
+++ C++/Medium/60.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i < 5;)
+    for (int i = 1; i < 5;i++)
     {
-        for (int j = 1 j <= 5 j++)
+        for (int j = 1; j <= 5; j++)
         {
-            cout << ;
+            cout << j << " ";
         }
         cout << endl;
     }
