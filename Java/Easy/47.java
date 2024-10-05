643252ac31ce3760ef99a01ffa76175b27d7bf1e
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 12:58:31 +0530

    47-java-easy

--- Java/Easy/47.java
+++ Java/Easy/47.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int a = 5;
-        int result = Math.pow(a);
+        double result = Math.pow(a,2);
         System.out.println(result);
     }
 }
