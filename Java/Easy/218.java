26349fcda9bb3f595403a823291a46390fcd1eca
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-05 13:58:56 +0530

    Update 218.java
    Java Easy 218

--- Java/Easy/218.java
+++ Java/Easy/218.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int x = 5;
-        double result = x / 0;
+        double result = x / 1;
         System.out.println(result);
     }
 }
