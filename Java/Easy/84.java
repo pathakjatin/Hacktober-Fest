65807ee80e1457bb1517246bd534020464af5ce6
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:43:59 +0530

    84-java-easy

--- Java/Easy/84.java
+++ Java/Easy/84.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int a = 0;
-        while (a = 10) {
+        while (a < 10) {
             System.out.println(a);
             a++;
         }
