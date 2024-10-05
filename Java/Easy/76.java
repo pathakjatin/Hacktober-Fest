4b29f5d9b2053be92fb1122c68dc6799d744cbef
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:33:10 +0530

    76-java-easy

--- Java/Easy/76.java
+++ Java/Easy/76.java
@@ -1,6 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++);
-        System.out.println(i);
+        for (int i = 0; i < 10; i++){
+
+            System.out.println(i);
+        }
     }
 }
