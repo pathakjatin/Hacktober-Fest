973ea0186793f092020df2ee44b6275a8b443ff9
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:48:30 +0530

    87-easy-java

--- Java/Easy/87.java
+++ Java/Easy/87.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        String message = 'Hello World';
+        String message = "Hello World";
         System.out.println(message);
     }
 }
