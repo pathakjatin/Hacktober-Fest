dcb33bc26599f458e7492d9c7cab9b5423af38ca
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:08:00 +0530

    solved 1

--- C++/Medium/1.cpp
+++ C++/Medium/1.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
+
 int main()
 {
-    std::string cars = {"Mercedes", "Audi", "Maruti", "Honda", "Ford"} std::cout << cars[0][] << std::endl;
+    std::string cars[5] = {"Mercedes", "Audi", "Maruti", "Honda", "Ford"};
+    std::cout << cars[0] << std::endl;
+
+    // string was not an array
     return 0;
 }
