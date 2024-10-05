0d43460419d09064cc906af1814c83788977429b
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:11:44 +0530

    61-easy-java

--- Java/Easy/61.java
+++ Java/Easy/61.java
@@ -1,8 +1,9 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        for (int i : arr) {
+        for (int i =0; i<arr.length;i++) {
             arr[i] = i * 2;
+            System.out.println(arr[i]);
         }
     }
 }
