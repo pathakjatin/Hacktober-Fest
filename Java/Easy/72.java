987161565354e92001f27849ed01f78503d1f7cc
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-05 13:28:47 +0530

    72-easy-java

--- Java/Easy/72.java
+++ Java/Easy/72.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         String[] names = {"John", "Doe"};
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
         }
     }
